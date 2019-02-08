87fe63505c9eeadb16f5857aba6bc35410a3e534
Kalidass08 <42038616+Kalidass08@users.noreply.github.com>
2019-02-08 16:54:52 +0530

    Create Positive.c

---
+++ Positive.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+	int a;
+	scanf("%d",&a);
+	if(a>=1)
+	{
+		printf("Positive");
+	}
+	else if(a==0)
+	{
+	 printf("Zero");	
+	}
+
+	else
+	printf("Negative");
+    return 0;
+}
