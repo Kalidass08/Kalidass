47a3a2e5dc31ffd63984e902071b2f6ec24c8804
Kalidass08 <42038616+Kalidass08@users.noreply.github.com>
2019-02-08 19:41:18 +0530

    Update Greatest.c

--- Greatest.c
+++ Greatest.c
@@ -3,11 +3,11 @@ int main()
 {
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
-    if(a>b && a>c)
+    if(a>=b && a>=c)
     {
         printf("%d",a);
     }
-    else if (b>a && b>c)
+    else if (b>=a && b>=c)
     {
         printf("%d",b);
     }
