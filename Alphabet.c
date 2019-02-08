5d115cdcf8f0ca4bdff66d12bebeb6d4da9deaa5
Kalidass08 <42038616+Kalidass08@users.noreply.github.com>
2019-02-08 19:25:55 +0530

    Create Alphabet.c

---
+++ Alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char n;
+    scanf("%c",&n);
+    if(n>='a' && n<='z')
+    {
+        printf("Alphabet");
+    }
+    else
+    printf("No");
+    return 0;
+}
+    
