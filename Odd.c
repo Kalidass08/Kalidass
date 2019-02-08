b350357280479f449cb4bbc755d678815fa01e1b
Kalidass08 <42038616+Kalidass08@users.noreply.github.com>
2019-02-08 17:39:11 +0530

    Update Odd.c

--- Odd.c
+++ Odd.c
@@ -1,1 +1,18 @@
 
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a%2==0)
+{
+    printf("Even");
+}
+else if (a%2==1)
+{
+    printf("Odd");
+}
+else
+printf("invalid");
+return 0;
+}
